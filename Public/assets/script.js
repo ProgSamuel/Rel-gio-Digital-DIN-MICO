dc1d30434eae933ea5ae080fe002a9b167535bd9
Samuel <progsamu@gmail.com>
2023-06-04 23:42:03 -0300

    Melhorias na responsividade e design
    Inclusão da responsividade para diversos aparelhos;
    Adicioando 3 opções de background além do padrão;

---
+++ Public/assets/script.js
@@ -0,0 +1,41 @@
+const horas =document.getElementById('horas');
+const minutos =document.getElementById('minutos');
+const segundos =document.getElementById('segundos');
+
+const relogio = setInterval(function time (){
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s  = dateToday.getSeconds();
+
+    if(hr < 10) hr = '0'+ hr;
+    if(min < 10) min = '0'+ min;
+    if(s < 10) s = '0'+ s;
+
+    horas.textContent =hr;
+    minutos.textContent = min;
+    segundos.textContent = s;
+})
+
+const divElemento = document.getElementById('meuDiv');
+const selectElement = document.getElementById('selectTema');
+
+selectElement.addEventListener('change', function() {
+  const selectedValue = selectElement.value;
+
+  switch (selectedValue) {
+    case 'PinkPop':
+      divElemento.style.background = 'linear-gradient(to left, #89fffd, #ef32d9)';
+      break;
+    case 'GrayBlue':
+        divElemento.style.background = 'linear-gradient(to left, #859398, #283048)';
+        break;
+    case 'Gray':
+        divElemento.style.background = 'linear-gradient(to left, #414345, #232526)'; 
+        break;
+    default:
+      divElemento.style.background = 'linear-gradient(120deg, #ffe53bd8, #ff2525da)'; // Valor padrão
+  }
+});
+
+/*    background: linear-gradient(120deg, #ffe53bd8, #ff2525da);  */
